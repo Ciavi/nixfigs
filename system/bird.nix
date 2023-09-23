1d5bacff24d809c767e407636a79078651a5ebc4
Matteo Ciavola <13613469+Ciavi@users.noreply.github.com>
2023-09-24 01:16:04 +0200

    Add pywalfox

--- system/bird.nix
+++ system/bird.nix
@@ -68,7 +68,25 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = with pkgs;
+  let
+    python-packages = ps: with ps; [
+      pip
+      (
+        buildPythonPackage rec {
+          pname = "pywalfox";
+          version = "2.7.4";
+          doCheck = false;
+          src = fetchPypi {
+            inherit pname version;
+            sha256 = "0rpdh1k4b37n0gcclr980vz9pw3ihhyy0d0nh3xp959q4xz3vrsr";
+          };
+        }
+      )
+      virtualenv
+    ];
+  in
+  [
      dotnet-sdk_7
      eza
      ffmpeg
@@ -83,8 +101,7 @@
      neofetch
      pciutils
      psmisc
-     python311
-     python311Packages.pip
+     (python311.withPackages python-packages)
      socat
      usbutils
   ];
